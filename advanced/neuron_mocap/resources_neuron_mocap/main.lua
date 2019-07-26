771661dba9e15520aadfd458871a8e511726e070
Konstantin S. Matveyev <kos@eligovision.ru>
2019-07-26 19:26:13 +0300

    neuron_mocap: mocapNode function added

--- advanced/neuron_mocap/resources_neuron_mocap/main.lua
+++ advanced/neuron_mocap/resources_neuron_mocap/main.lua
@@ -45,52 +45,58 @@ EV.Logger.setCatPriority("ev_evi.lua.neuronmocap"					, "INFO");
 EV.Logger.setCatPriority("ev_evi.lua.neuronmocap.registerPlugins"	, "INFO");
 EV.Logger.setCatPriority("ev_evi.lua.neuronmocap.server"			, "DEBUG");
 
-local skeletonNode = reactorController:getReactorByName("skeleton").node
-
-skeletonNode:setCullingActive(false)
+local skeletonNode1 = reactorController:getReactorByName("skeleton").node
+skeletonNode1:getOrCreateStateSet():setDefine("EV_GL_LIGHTING_DOUBLE_SIDED", osg.StateAttribute.ON)
+skeletonNode1:setCullingActive(false)
 
 local function calcBoneMatrix(x, y, z, ry, rx, rz)
 	-- TODO: optimize
 	-- NOTE: neuronmocap units are cm and degrees, we are in metres and radians
-	return	osg.Matrix.rotate(osg.Quat(rz/180.0*math.pi, osg.Vec3(0.0, 0.0, 1.0))
-							* osg.Quat(rx/180.0*math.pi, osg.Vec3(1.0, 0.0, 0.0))
-							* osg.Quat(ry/180.0*math.pi, osg.Vec3(0.0, 1.0, 0.0)))
+
+	return	osg.Matrix.rotate(osg.Quat(math.rad(rz), osg.Vec3(0.0, 0.0, 1.0))
+							* osg.Quat(math.rad(rx), osg.Vec3(1.0, 0.0, 0.0))
+							* osg.Quat(math.rad(ry), osg.Vec3(0.0, 1.0, 0.0)))
 			* osg.Matrix.translate(osg.Vec3(x, y, z)*0.01)
+
 end
 
-for i, boneInfo in ipairs(BonesInfo) do
-	local node = cast(EVosgUtil.findNamedClassNode(boneInfo.name, skeletonNode, "Bone"), osgAnimation.Bone)
-	if not node then
-		logger:warn("Cannot find bone '" .. boneName .. "'")
-		-- error()
-	else
-		logger:info("Bone '" .. boneInfo.name .. "' found!")
-		Bones[i] = { name = boneInfo.name, node = node, data = nil }
+local function mocapNode(skeletonNode)
+	for i, boneInfo in ipairs(BonesInfo) do
+		local node = cast(EVosgUtil.findNamedClassNode(boneInfo.name, skeletonNode, "Bone"), osgAnimation.Bone)
+		if not node then
+			logger:warn("Cannot find bone '" .. boneName .. "'")
+			-- error()
+		else
+			logger:info("Bone '" .. boneInfo.name .. "' found!")
+			Bones[i] = { name = boneInfo.name, node = node, data = nil }
 
-		local updateBone = osg.NodeCallback(function(aNode, aNodeVisitor)
-			local data = Bones[i].data
-			if not data then
-				return
-			end
+			local updateBone = osg.NodeCallback(function(aNode, aNodeVisitor)
+				local data = Bones[i].data
+				if not data then
+					return
+				end
 
-			local mat = calcBoneMatrix(data.x, data.y, data.z, data.ry, data.rx, data.rz)
+				local mat = calcBoneMatrix(data.x, data.y, data.z, data.ry, data.rx, data.rz)
 
-			node:setMatrix(mat)
+				node:setMatrix(mat)
 
-			local parent = node:getBoneParent()
-			if parent then
-				node:setMatrixInSkeletonSpace(mat*parent:getMatrixInSkeletonSpace())
-			else
-				node:setMatrixInSkeletonSpace(mat)
-			end
+				local parent = node:getBoneParent()
+				if parent then
+					node:setMatrixInSkeletonSpace(mat*parent:getMatrixInSkeletonSpace())
+				else
+					node:setMatrixInSkeletonSpace(mat)
+				end
 
-			Bones[i].mat = nil	-- wait for update
-		end)
+				Bones[i].mat = nil	-- wait for update
+			end)
 
-		node:setUpdateCallback(updateBone)
+			node:setUpdateCallback(updateBone)
+		end
 	end
 end
 
+mocapNode(skeletonNode1)
+viewer:getCamera():setComputeNearFarMode(osg.CullSettings.DO_NOT_COMPUTE_NEAR_FAR)
+
 createAndStartServer()
 
-viewer:getCamera():setComputeNearFarMode(osg.CullSettings.DO_NOT_COMPUTE_NEAR_FAR)
