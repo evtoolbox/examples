0bfdcfa0ab775c254cfa18a4e7f286e30314c6c7
Konstantin S. Matveyev <kos@eligovision.ru>
2020-04-08 20:26:49 +0300

    [*] osg_Geometry_example: some comments added

--- api/resources_api_examples/osg_Geometry_example.lua
+++ api/resources_api_examples/osg_Geometry_example.lua
@@ -1,4 +1,4 @@
--- Example of Geometry object сreation.
+-- Example of Geometry object creation.
 
 -- Creates regular polygon with <verticesCount> vertices and adds it to HUD.
 -- The resulting polygon is inscribed in a circle with a radius of <radius>.
@@ -6,7 +6,7 @@
 local verticesCount		= 6
 local radius			= 0.2
 local color				= osg.Vec4(1.0, 1.0, 1.0, 1.0)
-local lineWidth			= 3	-- NOTE: does not work on macOS
+local lineWidth			= 3		-- NOTE: Ignored on macOS (not supported in OpenGL Core Profile)
 local z					= 0.0
 
 local vertexArray = osg.Vec3Array(verticesCount)
@@ -16,15 +16,18 @@ local geometry = osg.Geometry()
 geometry:setUseVertexBufferObjects(true)
 geometry:setVertexArray(vertexArray)
 geometry:addPrimitiveSet(drawElements)
-geometry:setDataVariance(osg.Object.DYNAMIC)
+
+-- NOTE: Must be marked as DYNAMIC if you need to change vertexArray later
+-- geometry:setDataVariance(osg.Object.DYNAMIC)
 
 local uniform = osg.Uniform.Vec4f("ev_MaterialDiffuse", color)
+-- NOTE: Should be marked as DYNAMIC (for optimization purposes)
 uniform:setDataVariance(osg.Object.DYNAMIC)
 
 local stateSet = geometry:getOrCreateStateSet()
 stateSet:setAttributeAndModes(osg.LineWidth(lineWidth))
 stateSet:addUniform(uniform)
-stateSet:setMode(GLenum.GL_LIGHTING, osg.StateAttribute.OFF)
+stateSet:setMode(GLenum.GL_LIGHTING, osg.StateAttribute.OFF)	-- Disable lighting
 
 local step = 2*math.pi/verticesCount
 for i = 0, verticesCount - 1 do
@@ -37,8 +40,8 @@ for i = 0, verticesCount - 1 do
 	vertexArray:set(i, osg.Vec3(x, y, z))
 end
 
-geometry:setNodeMask(0xffffffff)
-coreProfileNode(geometry)
+-- geometry:setNodeMask(0xffffffff)	-- mask can be changed
+-- coreProfileNode(geometry)		-- no need (will be called before first Viewer's frame)
 
-local hud = reactorController:getReactorByName("HeadUpDisplay")
+local hud = reactorController:getReactorByName("HeadUpDisplay")	-- must be presented in the project
 hud.node:addChild(geometry)
