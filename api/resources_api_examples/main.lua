ae8f9579776d8dd8316ac66490d940a9cbdd7bcf
Igor Ivanov <laptabrok@gmail.com>
2020-04-16 13:40:18 +0300

    [*] Advanced API: RTT Camera example.

--- api/resources_api_examples/main.lua
+++ api/resources_api_examples/main.lua
@@ -4,4 +4,5 @@
 -- require "osg_Camera_example.lua"
 -- require "osg_PositionAttitudeTransform_example.lua"
 -- require "osg_MatrixTransform_example.lua"
-require "osg_NodeCallback_example.lua"
+-- require "osg_NodeCallback_example.lua"
+require "RTT_Camera_example.lua"
